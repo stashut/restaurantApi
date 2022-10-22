197d4f8cdc7aec0b0ca01a861884b8c7f035be8d
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-10-22 17:17:32 +0300

    moving functions to parent component

--- restaurant-app/src/components/Order/OrderForm.js
+++ restaurant-app/src/components/Order/OrderForm.js
@@ -2,34 +2,16 @@ import React, {useState} from 'react';
 import Form from "../layouts/Form"
 import {Grid} from "@material-ui/core";
 import { Input, Select, Button } from "../controls/Index"
-import {useForm} from "../hooks/useForm";
 
 const paymentMethods = [
     {id:'none', title:'Select'},
     {id:'Cash', title:'Cash'},
     {id:'Card', title:'Card'},
 ]
-const generateOrderNumber = () => Math.floor(100000 + Math.random() * 900000).toString();
-
-const getFreshModelObject = () => ({
-    orderMasterId: 0,
-    orderNumber: generateOrderNumber(),
-    customerId: 0,
-    paymentMethods: 'none',
-    grandTotal: 0,
-    deletedOrderItemIds:'',
-    orderDetails: []
-})
 
 function OrderForm(props) {
 
-    const {values,
-        setValues,
-        errors,
-        setErrors,
-        handleInputChange,
-        resetFormControls} = useForm(getFreshModelObject);
-
+    const { values, errors, handleInputChange } = props;
     return (
         <Form>
             <Grid container>
--- restaurant-app/src/components/Order/index.js
+++ restaurant-app/src/components/Order/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import OrderForm from "./OrderForm";
+import {useForm} from "../hooks/useForm";
+
+const generateOrderNumber = () => Math.floor(100000 + Math.random() * 900000).toString();
+
+const getFreshModelObject = () => ({
+    orderMasterId: 0,
+    orderNumber: generateOrderNumber(),
+    customerId: 0,
+    paymentMethods: 'none',
+    grandTotal: 0,
+    deletedOrderItemIds:'',
+    orderDetails: []
+})
 
 function Order(props) {
+
+    const {values, setValues, errors, setErrors, handleInputChange, resetFormControls} = useForm(getFreshModelObject);
+
     return (
-        <OrderForm></OrderForm>
+        <OrderForm
+            { ...{values, errors, handleInputChange}}
+        />
     );
 }
 
