cd124bc5b631827850a1bb5a4ad2c7ca057ba196
Stanislav Hut <stanislav.hut@solarwinds.com>
2022-10-22 16:09:12 +0300

    Front-end initialized

---
+++ restaurant-app/src/components/Order/OrderForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function OrderForm(props) {
+    return (
+        <form noValidate autoComplete="off">
+            <input />
+            <select />
+        </form>
+    );
+}
+
+export default OrderForm;
---
+++ restaurant-app/src/components/Order/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import OrderForm from "./OrderForm";
+
+function Order(props) {
+    return (
+        <OrderForm></OrderForm>
+    );
+}
+
+export default Order;
